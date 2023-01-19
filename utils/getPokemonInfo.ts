bc1cf212e41d89d6645569ed4b72569b17ca62ac
Manuel Ulate <fr3zs09@gmail.com>
2023-01-19 16:27:46 -0600

    implementando ISG

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -3,14 +3,20 @@ import { PokemonResponse } from "types";
 
 export const getPokemonInfo = async ( nameOrdId: string ) => {
 
-    const { data } = await pokeApi.get<PokemonResponse>(`/pokemon/${ nameOrdId }`);
-    
-     
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+    try {
+        
+        const { data } = await pokeApi.get<PokemonResponse>(`/pokemon/${ nameOrdId }`);
+         
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
+
+    } catch ( error ) {
+        return null;
     }
+    
 
 
 }
