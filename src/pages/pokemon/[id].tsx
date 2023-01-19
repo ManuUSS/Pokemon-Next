8d52db2b0b486af64a027126e26f07c9815bd1b0
Manuel Ulate <fr3zs09@gmail.com>
2023-01-18 21:40:30 -0600

    generando paginas de pokemones

--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -1,18 +1,49 @@
+import { pokeApi } from "api";
+import { Layout } from "components/layouts";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
-import { SmallPokemon } from 'types';
+import { PokemonResponse } from 'types';
 
 
 interface Props {
-    pokemon: SmallPokemon
+    pokemon: PokemonResponse
+    
 }
 
-const PokemonDetail = () => {
+const PokemonDetail:NextPage<Props> = ({ pokemon }) => {
 
     const { query } = useRouter();
 
   return (
-    <div>PokemonDetail</div>
+    <Layout title="Algún Pokemón">
+        <h1>{ pokemon.name }</h1>
+    </Layout>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+    
+    const totalPokemons = [ ...Array( 151 ) ].map( ( value, index ) => `${ index + 1 }` );
+
+    return {
+        paths: totalPokemons.map( ( id ) => ({
+            params: { id }
+        })),
+        fallback: false
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    
+    const { id } = params as { id: string };
+
+    const { data } = await pokeApi.get<PokemonResponse>(`/pokemon/${ id }`);
+    
+    return {
+        props: {
+            pokemon: data
+        }
+    }
 }
 
 export default PokemonDetail;
